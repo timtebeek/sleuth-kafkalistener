c3703c9c4b4c30929a72d98868c894517dd44e59
Tim te Beek <tim.te.beek@ing.nl>
2018-09-02 15:50:26 +0200

    Drop scheduled message for clarity

--- src/main/java/com/github/timtebeek/sleuth/kafkalistener/SleuthKafkalistenerApplication.java
+++ src/main/java/com/github/timtebeek/sleuth/kafkalistener/SleuthKafkalistenerApplication.java
@@ -1,21 +1,11 @@
 package com.github.timtebeek.sleuth.kafkalistener;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
-@EnableScheduling
-@Slf4j
 public class SleuthKafkalistenerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SleuthKafkalistenerApplication.class, args);
-    }
-
-    @Scheduled(fixedRate = 10)
-    public void logSomethingFromAnotherThread() {
-        log.info("Periodic log message");
     }
 }
